1bd64fe001b150809377c49d9f79f610abd5964a
Fredxd <frederic@appolo.fr>
2026-03-05 10:54:35 +0100

    fix(BUG-05): filter panel state via sessionStorage, remove _reset param

--- assets/js/controllers/filter_panel_controller.js
+++ assets/js/controllers/filter_panel_controller.js
@@ -3,12 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ['panel', 'toggle']
 
+    get _storageKey() {
+        return `filter-panel:${window.location.pathname}`
+    }
+
     connect() {
-        // Auto-open if filter params (other than q/page) are present in URL
         const url = new URL(window.location.href)
         const hasActive = [...url.searchParams.entries()]
             .some(([key, val]) => !['q', 'page'].includes(key) && val !== '')
-        if (hasActive && this.hasPanelTarget) {
+        const storedOpen = sessionStorage.getItem(this._storageKey) === '1'
+
+        if ((hasActive || storedOpen) && this.hasPanelTarget) {
             this._setOpen(true)
         }
     }
@@ -21,6 +26,11 @@ export default class extends Controller {
         this.panelTarget.classList.toggle('hidden', !open)
         if (this.hasToggleTarget) {
             this.toggleTarget.toggleAttribute('data-active', open)
+        }
+        if (open) {
+            sessionStorage.setItem(this._storageKey, '1')
+        } else {
+            sessionStorage.removeItem(this._storageKey)
         }
     }
 }
